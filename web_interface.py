9ca8f957add42b64cfb9224c2d3a61c51006c862
Charles <dolphinsandfriends@gmail.com>
2013-08-30 22:59:12 -0500

    Added comments and error message

--- web_interface.py
+++ web_interface.py
@@ -13,25 +13,28 @@ conn = sqlite3.connect("apis.db")
 def query():
 	try:
 		c = conn.cursor()
+		# Print out the entire database
 		return str(c.execute("SELECT * FROM apis").fetchall())
 	except:
-		return
+		return "Something went wrong..."
 
 # apropros.com/register_api/api_name&tag=...
 @interface.route("/register_api/<api_name>")
 def register_api(api_name):
 	try:
+		# Returns all values with "tag" as the key
 		return str(dict(request.args)["tag"])
 	except:
-		return
+		return "Something went wrong..."
 
 # apropros.com/drop_api/api_name
 @interface.route("/drop_api/<api_name>")
 def drop_api(api_name):
 	try:
+		# sqlite implementation to be implemented
 		return "Delete " + api_name + " from the database"
 	except:
-		return
+		return "Something went wrong..."
 
 # Run only if called in this script
 if __name__ == "__main__":
