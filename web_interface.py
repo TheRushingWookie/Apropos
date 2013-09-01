805ca963a73bf2f888f0912e15714471ed8df543
Charles <dolphinsandfriends@gmail.com>
2013-08-31 20:24:49 -0500

    Added email script

--- web_interface.py
+++ web_interface.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask import request
 import sqlite3
+import json
 
 # Create flask object
 interface = Flask(__name__)
@@ -10,26 +13,36 @@ conn = sqlite3.connect("apis.db")
 
 # apropros.com/query
 @interface.route("/query")
-def query():
+def web_query():
 	try:
+		tags = dict(request.args)["tag"]
+		database = str(c.execute("SELECT * FROM apis").fetchall())
 		c = conn.cursor()
-		# Print out the entire database
-		return str(c.execute("SELECT * FROM apis").fetchall())
+		
+	except:
+		return "Something went wrong..."
+
+@interface.route("/register_api_provider/<api_provider>")
+def web_register_api_provider(api_provider):
+	# to-do: check if provider already exists
+	try:
+
+		return str(str(api_provider) + str(dict(request.args)["contact_info"]))
 	except:
 		return "Something went wrong..."
 
 # apropros.com/register_api/api_name&tag=...
 @interface.route("/register_api/<api_name>")
-def register_api(api_name):
+def web_register_api(api_name):
 	try:
 		# Returns all values that have "tag" as the key
-		return str(dict(request.args)["tag"])
+		return str(str(api_name) + str(dict(request.args)["tag"]))
 	except:
 		return "Something went wrong..."
 
 # apropros.com/drop_api/api_name
 @interface.route("/drop_api/<api_name>")
-def drop_api(api_name):
+def web_drop_api(api_name):
 	try:
 		# sqlite implementation to be implemented
 		return "Delete " + api_name + " from the database"
