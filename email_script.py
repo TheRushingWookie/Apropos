653111a7a917f7fc8a779b85bca45649b4f01bf4
Charles <dolphinsandfriends@gmail.com>
2013-08-31 21:01:21 -0500

    Added email_script.py

---
+++ email_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import smtplib
+
+def send_email(user, password, user_address, receiver):     
+    # Initialize SMTP server
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(user, password)
+    
+    # Send email    
+    server.sendmail(user_address, receiver, "GENERATED KEY")
+    server.quit()
