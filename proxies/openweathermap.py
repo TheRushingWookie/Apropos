f28059d57801822930241708d5fc2ad9876b1f12
Quinn Jarrell <qjarrell@gosynapsify.com>
2013-10-15 15:50:12 -0500

    Added sample output functions to openweathermap.py, might want to switch the json to using purely standard types and then have a generalized converter function instead of repeating the same series of if elses to convert stuff. Also added a function to run through the output params and call the appropriate functs in the proxy class

--- proxies/openweathermap.py
+++ proxies/openweathermap.py
@@ -2,17 +2,54 @@ import json
 import urllib2
 def init_actions():
 	return {'weather':[__name__,'get_weather']}
-def get_weather(json):
-	print('getweather')
+def get_weather(json_input):
+	
 	base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-	if 'city' in json:
-		base_url += 'q=' + json['city']
-	elif 'latitude' in json and 'longitude' in json:
-		base_url += 'lat=' + json[latitude] + '&lon=' + json[longitude]
+	if 'city' in json_input:
+		base_url += 'q=' + json_input['city']
+	elif 'latitude' in json_input and 'longitude' in json_input:
+		base_url += 'lat=' + json_input[latitude] + '&lon=' + json_input[longitude]
 	
 
 
-	json = urllib2.urlopen(base_url)
+	json_output = urllib2.urlopen(base_url).read()
 	
-	return str(json.read())
-#get_weather({'city':'Bethesda, MD'})
+	return json_output
+def init_outputs():
+	return {'temperature' : output_temperature,
+			'pressure' : output_pressure,
+			'windspeed' : output_windspeed}
+def output_temperature(json_output,val_type):
+	temp =  json_output['main']['temp']
+	temp = convert_kelvin_to_fahrenheit(temp)
+	print val_type
+	if val_type == 'int':
+		return int(temp)
+	elif val_type == 'string':
+		return str(temp)
+	elif val_type == 'float':
+		return float(temp)
+def output_pressure(json_output,val_type):
+	temp =  json_output['main']['pressure']
+	temp = convert_kelvin_to_fahrenheit(temp)
+	print val_type
+	if val_type == 'int':
+		return int(temp)
+	elif val_type == 'string':
+		return str(temp)
+	elif val_type == 'float':
+		return float(temp)
+def output_windspeed(json_output,val_type):
+	temp =  json_output['wind']['speed']
+	temp = convert_kelvin_to_fahrenheit(temp)
+	print val_type
+	if val_type == 'int':
+		return int(temp)
+	elif val_type == 'string':
+		return str(temp)
+	elif val_type == 'float':
+		return float(temp)
+
+#print get_weather({'city':'Bethesda, MD'})
+def convert_kelvin_to_fahrenheit(Kelvin_temp):
+	return (Kelvin_temp - 273.15) * 1.8 + 32
--- proxy.py
+++ proxy.py
@@ -10,7 +10,7 @@ from os.path import *
 import sys
 interface = Flask(__name__)
 actions = {}
-
+json_outputs = {}
 @interface.route("/query")
 def query():
 
@@ -23,9 +23,9 @@ def query():
 	
 	io_json_dict = json.loads(io_json_list[0])
 	io_json_dict = json.loads(io_json_dict)
-	io_json_dict = json.loads(io_json_dict)
+	
 	action = io_json_dict['action']
-	print action
+	print actions
 	if action:
 		print(str(actions))
 		funct_name = actions[action]
@@ -33,8 +33,22 @@ def query():
 		funct = get_funct(funct_name[1],funct_name[0])
 		print str(funct)
 		return funct(io_json_dict)
+def filter_outputs (json_input,output):
+	filtered_json = {}
+
+	for i in json_input['output'].keys():
+		print json_outputs[i]
+		try:
+			funct = json_outputs[i]
+
+			print funct
+			filtered_json[i] = funct(output,json_input['output'][i])
+		except:
+			return json.dumps({'wrong_outputs':i})
+	return json.dumps(filtered_json)
+
 def run_proxy(proxy_name):
-	global actions
+	global actions,json_outputs
 	dir = os.path.abspath(inspect.getsourcefile(run_proxy))
 	dir =  os.path.dirname(dir)
 	
@@ -43,7 +57,8 @@ def run_proxy(proxy_name):
 	try:
 		func = getattr(mod, "init_actions")
 		actions = func()
-		
+		func = getattr(mod, "init_outputs")
+		json_outputs = func()
 	except AttributeError:
 		print 'function not found ' + "init_actions"
 	
@@ -55,9 +70,49 @@ def get_funct(funct_name,package_name):
 		print 'function not found ' + funct_name
 	else:
 		return func
-
-if __name__ == "__main__":
+run_proxy('openweathermap')
+print filter_outputs(json.loads('{"output": {"temperature": "string","pressure":"float", "windspeed":"int"}}'),json.loads('''{
+    "coord": {
+        "lon": -77.0969,
+        "lat": 38.9864
+    },
+    "sys": {
+        "country": "United States of America",
+        "sunrise": 1381835926,
+        "sunset": 1381876149
+    },
+    "weather": [
+        {
+            "id": 801,
+            "main": "Clouds",
+            "description": "few clouds",
+            "icon": "02d"
+        }
+    ],
+    "base": "gdps stations",
+    "main": {
+        "temp": 292.808,
+        "temp_min": 292.708,
+        "temp_max": 292.708,
+        "pressure": 1027.29,
+        "sea_level": 1034.89,
+        "grnd_level": 1027.29,
+        "humidity": 71
+    },
+    "wind": {
+        "speed": 1.95,
+        "deg": 42.5005
+    },
+    "clouds": {
+        "all": 20
+    },
+    "dt": 1381865209,
+    "id": 4348599,
+    "name": "Bethesda",
+    "cod": 200
+}'''))
+'''if __name__ == "__main__":
 	run_proxy('openweathermap')
 	print str(actions)
-	interface.run(port=8000)
+	interface.run(port=8000)'''
 
