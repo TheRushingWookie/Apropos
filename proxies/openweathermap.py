638acf5ecff909747e84d0de574b69a31397c66a
Quinn Jarrell <qjarrell@gosynapsify.com>
2013-10-16 15:39:05 -0500

    switched to object model for proxies

--- proxies/openweathermap.py
+++ proxies/openweathermap.py
@@ -1,55 +1,61 @@
 import json
 import urllib2
-def init_actions():
-	return {'weather':[__name__,'get_weather']}
-def get_weather(json_input):
-	
-	base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-	if 'city' in json_input:
-		base_url += 'q=' + json_input['city']
-	elif 'latitude' in json_input and 'longitude' in json_input:
-		base_url += 'lat=' + json_input[latitude] + '&lon=' + json_input[longitude]
-	
+import sys
+import proxy
+class openweathermap(proxy.proxy):
+	def __init__ (self):
+		self.actions = self.init_actions()
+		self.json_outputs = self.init_outputs()
+	def init_actions(self):
+		return {'weather':[__name__,'get_weather']}
+	def get_weather(self,json_input):
+		
+		base_url = 'http://api.openweathermap.org/data/2.5/weather?'
+		if 'city' in json_input:
+			base_url += 'q=' + json_input['city']
+		elif 'latitude' in json_input and 'longitude' in json_input:
+			base_url += 'lat=' + json_input[latitude] + '&lon=' + json_input[longitude]
+		
 
 
-	json_output = urllib2.urlopen(base_url).read()
-	
-	return json_output
-def init_outputs():
-	return {'temperature' : output_temperature,
-			'pressure' : output_pressure,
-			'windspeed' : output_windspeed}
-def output_temperature(json_output,val_type):
-	temp =  json_output['main']['temp']
-	temp = convert_kelvin_to_fahrenheit(temp)
-	print val_type
-	if val_type == 'int':
-		return int(temp)
-	elif val_type == 'string':
-		return str(temp)
-	elif val_type == 'float':
-		return float(temp)
-def output_pressure(json_output,val_type):
-	temp =  json_output['main']['pressure']
-	temp = convert_kelvin_to_fahrenheit(temp)
-	print val_type
-	if val_type == 'int':
-		return int(temp)
-	elif val_type == 'string':
-		return str(temp)
-	elif val_type == 'float':
-		return float(temp)
-def output_windspeed(json_output,val_type):
-	temp =  json_output['wind']['speed']
-	temp = convert_kelvin_to_fahrenheit(temp)
-	print val_type
-	if val_type == 'int':
-		return int(temp)
-	elif val_type == 'string':
-		return str(temp)
-	elif val_type == 'float':
-		return float(temp)
+		self.json_output = urllib2.urlopen(base_url).read()
+		
+		return json_output
+	def init_outputs(self):
+		return {'temperature' : self.output_temperature,
+				'pressure' : self.output_pressure,
+				'windspeed' : self.output_windspeed}
+	def output_temperature(self,json_output,val_type):
+		temp =  self.json_output['main']['temp']
+		temp = self.convert_kelvin_to_fahrenheit(temp)
+		print val_type
+		if val_type == 'int':
+			return int(temp)
+		elif val_type == 'string':
+			return str(temp)
+		elif val_type == 'float':
+			return float(temp)
+	def output_pressure(self,json_output,val_type):
+		temp =  self.json_output['main']['pressure']
+		
+		print val_type
+		if val_type == 'int':
+			return int(temp)
+		elif val_type == 'string':
+			return str(temp)
+		elif val_type == 'float':
+			return float(temp)
+	def output_windspeed(self,json_output,val_type):
+		temp =  self.json_output['wind']['speed']
+		
+		print val_type
+		if val_type == 'int':
+			return int(temp)
+		elif val_type == 'string':
+			return str(temp)
+		elif val_type == 'float':
+			return float(temp)
 
-#print get_weather({'city':'Bethesda, MD'})
-def convert_kelvin_to_fahrenheit(Kelvin_temp):
-	return (Kelvin_temp - 273.15) * 1.8 + 32
+	#print get_weather({'city':'Bethesda, MD'})
+	def convert_kelvin_to_fahrenheit(self,Kelvin_temp):
+		return (Kelvin_temp - 273.15) * 1.8 + 32
--- proxy.py
+++ proxy.py
@@ -8,111 +8,76 @@ import os
 import inspect
 from os.path import *
 import sys
-interface = Flask(__name__)
-actions = {}
-json_outputs = {}
-@interface.route("/query")
-def query():
+
+class proxy():
+	interface = Flask(__name__)
+	actions = {}
+	json_outputs = {}
+
+	@interface.route("/query")
+	def query(self):
+
+		
+		immutable_multi_dict = request.args
+		
+		norm_dict = dict(immutable_multi_dict)
+		
+		io_json_list = norm_dict['json']
+		
+		io_json_dict = json.loads(io_json_list[0])
+		io_json_dict = json.loads(io_json_dict)
+		
+		action = io_json_dict['action']
+		print actions
+		if action:
+			print(str(self.actions))
+			funct = self.actions[action]
+			
+			print str(funct)
+			return funct(io_json_dict)
+	def filter_outputs (self,json_input,output):
+		filtered_json = {}
+
+		for i in json_input['output'].keys():
+			print json_outputs[i]
+			try:
+				funct = json_outputs[i]
+
+				print funct
+				filtered_json[i] = funct(output,json_input['output'][i])
+			except:
+				return json.dumps({'wrong_outputs':i})
+		return json.dumps(filtered_json)
 
 	
-	immutable_multi_dict = request.args
-	
-	norm_dict = dict(immutable_multi_dict)
-	
-	io_json_list = norm_dict['json']
-	
-	io_json_dict = json.loads(io_json_list[0])
-	io_json_dict = json.loads(io_json_dict)
-	
-	action = io_json_dict['action']
-	print actions
-	if action:
-		print(str(actions))
-		funct_name = actions[action]
-		print str(funct_name)
-		funct = get_funct(funct_name[1],funct_name[0])
-		print str(funct)
-		return funct(io_json_dict)
-def filter_outputs (json_input,output):
-	filtered_json = {}
+		
 
-	for i in json_input['output'].keys():
-		print json_outputs[i]
+	def get_funct(self,funct_name,package_name):
 		try:
-			funct = json_outputs[i]
-
-			print funct
-			filtered_json[i] = funct(output,json_input['output'][i])
-		except:
-			return json.dumps({'wrong_outputs':i})
-	return json.dumps(filtered_json)
-
+			func = getattr(sys.modules[package_name], funct_name)
+		except AttributeError:
+			print 'function not found ' + funct_name
+		else:
+			return func
 def run_proxy(proxy_name):
 	global actions,json_outputs
 	dir = os.path.abspath(inspect.getsourcefile(run_proxy))
 	dir =  os.path.dirname(dir)
-	
+	sys.path.append(dir)
 	mod = importlib.import_module("proxies." + proxy_name)
+
 	print str(mod.__name__)
 	try:
-		func = getattr(mod, "init_actions")
-		actions = func()
-		func = getattr(mod, "init_outputs")
-		json_outputs = func()
+		proxy_instance = getattr(mod, proxy_name)()
+		print proxy_instance.actions
+		
+
+		return proxy_instance
 	except AttributeError:
 		print 'function not found ' + "init_actions"
-	
+if __name__ == "__main__":
+	proxy_instance = run_proxy('openweathermap')
+	proxy_instance.get_weather()
+	proxy_instance.interface.run(port=8000)
 
-def get_funct(funct_name,package_name):
-	try:
-		func = getattr(sys.modules[package_name], funct_name)
-	except AttributeError:
-		print 'function not found ' + funct_name
-	else:
-		return func
-run_proxy('openweathermap')
-print filter_outputs(json.loads('{"output": {"temperature": "string","pressure":"float", "windspeed":"int"}}'),json.loads('''{
-    "coord": {
-        "lon": -77.0969,
-        "lat": 38.9864
-    },
-    "sys": {
-        "country": "United States of America",
-        "sunrise": 1381835926,
-        "sunset": 1381876149
-    },
-    "weather": [
-        {
-            "id": 801,
-            "main": "Clouds",
-            "description": "few clouds",
-            "icon": "02d"
-        }
-    ],
-    "base": "gdps stations",
-    "main": {
-        "temp": 292.808,
-        "temp_min": 292.708,
-        "temp_max": 292.708,
-        "pressure": 1027.29,
-        "sea_level": 1034.89,
-        "grnd_level": 1027.29,
-        "humidity": 71
-    },
-    "wind": {
-        "speed": 1.95,
-        "deg": 42.5005
-    },
-    "clouds": {
-        "all": 20
-    },
-    "dt": 1381865209,
-    "id": 4348599,
-    "name": "Bethesda",
-    "cod": 200
-}'''))
-'''if __name__ == "__main__":
-	run_proxy('openweathermap')
-	print str(actions)
-	interface.run(port=8000)'''
 
