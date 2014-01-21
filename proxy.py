26f8ba7fc47875b5e0dab25664a1d2eb8cabb532
Quinn Jarrell <qjarrell@gosynapsify.com>
2014-01-20 20:53:11 -0600

    readded standard_type_converter

--- proxy.py
+++ proxy.py
@@ -12,4 +12,22 @@ import sys
 
 class proxy():
 	interface = Flask(__name__)
-	def query(query_json)
+
+	def __init__ (self):
+		pass
+	def standard_type_converter(self,val,val_type):
+		'''Converts all standard types such as integer, string'''
+		val_type = val_type.lower()
+		print "val_type " + val_type + ' ' + str(val)
+		try:
+			if val_type == 'int' or val_type == 'integer':
+				if val.find('.'):
+					return int(float(val))	
+				return int(val)
+			elif val_type == 'string':
+				return str(val)
+			elif val_type == 'float':
+				return float(val)
+		except:
+			print "Bad conversion of " + str(val) + " + with type " + str(val_type) #  Might want to make str(val) keep escaped characters
+		return val
