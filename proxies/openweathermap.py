f28983da79d00c42f750d5a0048336ab0ec5eb2f
Charles <Charles@pbd2892.urh.uiuc.edu>
2014-03-05 23:38:48 -0600

    openweathermap proxy kinda better

--- proxies/openweathermap.py
+++ proxies/openweathermap.py
@@ -1,30 +1,42 @@
 import json
-import urllib2
-import sys
+import requests
 import proxy
+
 class openweathermap(proxy.proxy):
 
-							
-	json_name_to_path_map = {'temperature': ['main', 'temp'], 'temperature max': ['main', 'temp_max'], 'humidity': ['main', 'humidity'], 'pressure': ['main', 'pressure'], 'temperature minimum': ['main', 'temp_min'], 'cloudiness': ['clouds', 'all'], 'windspeed': ['wind', 'speed'], 'wind direction': ['wind', 'deg']}
-	
+    json_name_to_path_map = {'temperature': ['main', 'temp'], 'temperature max': ['main', 'temp_max'], 'humidity': ['main', 'humidity'], 'pressure': ['main', 'pressure'], 'temperature minimum': ['main', 'temp_min'], 'cloudiness': ['clouds', 'all'], 'windspeed': ['wind', 'speed'], 'wind direction': ['wind', 'deg']}
 
-	def get_weather(self,json_input):
-		
-		base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-		if 'city' in json_input['input']:
-			base_url += 'q=' + json_input['input']['city']
-		elif 'latitude' in json_input and 'longitude' in json_input['input']:
-			base_url += 'lat=' + json_input['input'][latitude] + '&lon=' + json_input['input'][longitude]
-		self.logger.debug('base_url %s', base_url)
-		req = urllib2.Request(base_url)
-		req.add_unredirected_header('User-Agent', 'Apropos')
-		json_output = urllib2.urlopen(req).read()	
-		self.logger.debug("json output is %s", json_output)
-		return json.loads(json_output)
-			
 
-	#print get_weather({'city':'Bethesda, MD'})
-	def convert_kelvin_to_fahrenheit(self,Kelvin_temp):
-		return (Kelvin_temp - 273.15) * 1.8 + 32
-	def init_actions(self):
-		return {'weather':self.get_weather}
+    def get_weather(self, json_input):
+        """
+        Usage:
+        print get_weather({'input': {'city': 'Bethesda, MD'}})
+        """
+
+        url = 'http://api.openweathermap.org/data/2.5/weather?'
+
+        # Client has city as input
+        if 'city' in json_input['input']:
+            payload = {'q': json_input['input']['city']}
+
+        # Client has longitude and latitude as input
+        elif 'latitude' in json_input and 'longitude' in json_input['input']:
+            payload = {'lat': json_input['input']['latitude'],
+                       'lon': json_input['input']['longitude']}
+
+        if payload:
+            r = requests.get(url, params=payload, headers={'User-Agent': 'Apropos'})
+            if r.status_code == 200:
+                return r.json()
+            else:
+                raise Exception('Status code: {0}'.format(r.status_code))
+        else:
+            raise Exception('Invalid tags')
+
+
+    def convert_kelvin_to_fahrenheit(self, Kelvin_temp):
+        return (Kelvin_temp - 273.15) * 1.8 + 32
+
+
+    def init_actions(self):
+        return {'weather':self.get_weather}
