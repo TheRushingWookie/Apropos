5d7c5beb678a8c1e4e44637b6282c14d563eb0aa
Charles <Charles@mobile-172-16-108-128.near.illinois.edu>
2014-03-19 17:12:42 -0500

    small fixes

--- Analytics/api_analytics.py
+++ Analytics/api_analytics.py
@@ -1,6 +1,9 @@
 from main import *
+
+
 class api_analytic(db.Model):
-	__tablename__ = 'api_analytics'
-	id = db.Column(db.Integer,primary_key=True)
-	api_id = db.Column(db.Integer,db.ForeignKey('api_endpoints.id'))
-	api = db.relationship("api_endpoints", backref=db.backref('api_analytics', order_by=id))
+    __tablename__ = 'api_analytics'
+    id = db.Column(db.Integer, primary_key=True)
+    api_id = db.Column(db.Integer, db.ForeignKey('api_endpoints.id'))
+    api = db.relationship(
+        "api_endpoints", backref=db.backref('api_analytics', order_by=id))
--- Analytics/user.py
+++ Analytics/user.py
@@ -1,14 +1,14 @@
-######################################################
 # Analytics
-######################################################
 from main import *
 
 
 class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer,primary_key=True)
-	ip = db.Column(db.Text)
-	def __init__(self, ip):
-		self.ip = ip
-	def __repr__(self):
-		return ip
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.Text)
+
+    def __init__(self, ip):
+        self.ip = ip
+
+    def __repr__(self):
+        return ip
